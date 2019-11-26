2bf058974ff9471ec5af43a7e907397cdf747fd6
Naaaavii <ivanoloko1000@gmail.com>
2019-11-25 22:12:50 -0300

    functions
    safe

---
+++ py/functions.py
@@ -0,0 +1,22 @@
+#def potenciação(a,b):
+ #   print(a**b)
+
+#potenciação(5,8)
+
+#def soma (a,b):
+#    print(a)
+#    print(b)
+#    print(f"{a} + {b} é igual a:")
+#    print(a + b)
+#soma(4,9)
+
+def soma (a, b):
+    return a + b # return, retorna os valores
+
+def multiplicação (a,b):
+    return a * b
+
+s = soma(2, 8) # return precisa de valores armazenados em uma variavel
+m = multiplicação(5, 5)
+print(m)
+print(s)
