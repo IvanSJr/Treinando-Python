43c3718577ba5404988bdeda285838a14d226631
Naaaavii <ivanoloko1000@gmail.com>
2019-11-26 20:45:32 -0300

    aleatorio.py

---
+++ py/aleatorio.py
@@ -0,0 +1,8 @@
+import random #importa o modulo
+
+numero = random.randint(9,50) #escolhe os número aleatoriamente
+print(numero)
+
+lista = [8, 9, 852, 5, 682, 9] 
+numero = random.choice(lista) #escolhe um dos valorers da lista
+print(numero)
