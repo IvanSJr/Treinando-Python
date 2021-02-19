c99bdad6c860abbe18871d343e335afce4308f44
IvanSJr <ivanjrjesus01@gmail.com>
2021-02-19 09:39:45 -0300

    Adicionando a função join ao repositório.

---
+++ python_basic/join.py
@@ -0,0 +1,9 @@
+"""
+Função join - Junta uma lista
+"""
+
+string = 'O Brasil é penta'
+lista = string.split()
+print(lista)  # ['O', 'Brasil', 'é', 'penta']
+string2 = ','.join(lista)
+print(string2)
