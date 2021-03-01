5a5d2d6c4173d8d98031440fde1fdfef12b6f1d7
IvanSJr <ivanjrjesus01@gmail.com>
2021-03-01 12:01:13 -0300

    Atualizando arquivo de contadores

--- python_basic/contadores.py
+++ python_basic/contadores.py
@@ -1,2 +1,15 @@
-for p, r in enumerate(range(10, 1, -1)):
-    print(p, r)
+from itertools import count
+
+contador = count(start=1, step=-1)  # 0 -> -1 -> -2
+contador = count(start=1, step=1)  # 1 -> 2 -> 3
+for i in contador:
+    if i != 10:
+        print(i)
+    else:
+        break
+
+# Adicionando indices a listas usando o count
+contador_lista = count(start=1)
+lista = ['João', 'Maria', 'Felipe', 'Flávio']
+index_lista = zip(contador_lista, lista)
+print(list(index_lista))
