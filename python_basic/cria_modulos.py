932fec2ac8267c4c37214a3d4de4badf4e770ccc
IvanSJr <ivanjrjesus01@gmail.com>
2021-03-05 14:04:28 -0300

    Criando módulos em python

---
+++ python_basic/cria_modulos.py
@@ -0,0 +1,9 @@
+from calculos import multiplica  # Chamando o arquivo de funções(modulos)
+# [0, 2, 4, 6, 8, 10]
+# 120
+# 3.141592653589793
+# calculos é o nome do módulo que foi importado
+# print(__name__) __main__ é o nome desse modulo
+
+lista = [2, 4, 6, 8, 10]
+print(multiplica(lista))
