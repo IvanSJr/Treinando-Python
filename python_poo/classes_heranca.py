83f778f56bab4919a6b9f7ac4f8ff4618a14cb87
IvanSJr <ivanjrjesus01@gmail.com>
2021-03-14 23:50:49 -0300

    Um pouco sobre sobreposição de metodos em python 3.

--- python_poo/classes_heranca.py
+++ python_poo/classes_heranca.py
@@ -23,13 +23,28 @@ class Aluno(Pessoa):
         print(f'{self.nome} está estudando')
 
 
-cliente1 = Cliente('João', 12)
-aluno1 = Aluno('Ivan', 19)
-p1 = Pessoa('Felipe', 50)
+class ClienteVip(Cliente):
+    def __init__(self, nome, idade, sobrenome):
+        super().__init__(nome, idade)
+        self.sobrenome = sobrenome.title()
 
-p1.falar()
-aluno1.falar()
-cliente1.falar()
+    def falar(self):
+        super().falar()  # Utilizando o metodo falar() da classe pai(Cliente). Busca o primeiro metodo que aparecer
+        print(f'{self.nome_da_classe.lower()} está executando a função falar')
+        print(f'{self.sobrenome}, {self.nome}. Idade: {self.idade}')
 
-aluno1.estudar()
-cliente1.comprar()
+
+# Sobreposição de classes
+c1 = ClienteVip('Ivan', 50, 'Santos')
+c1.falar()
+
+# cliente1 = Cliente('João', 12)
+# aluno1 = Aluno('Ivan', 19)
+# p1 = Pessoa('Felipe', 50)
+#
+# p1.falar()
+# aluno1.falar()
+# cliente1.falar()
+#
+# aluno1.estudar()
+# cliente1.comprar()
